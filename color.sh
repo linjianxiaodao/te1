b9dc338b8e4dee0e50bc220c802b133ecbcfff40
fumimi <fumm@haplox.com>
2018-06-22 16:47:45 +0800

    color

---
+++ color.sh
@@ -0,0 +1,9 @@
+
+n rmsg() { echo -e "\033[31;49m$*\033[0m"; }
+  3 gmsg() { echo -e "\033[32;49m$*\033[0m"; }
+  4 bmsg() { echo -e "\033[34;49m$*\033[0m"; }
+  5 
+  6 rmsg red
+  7 gmsg green
+  8 bmsg blue
+
