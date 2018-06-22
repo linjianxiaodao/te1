6df76c2e64acfcc8d7c8fefe1c2acc623298479e
linjianxiaodao <39516876+linjianxiaodao@users.noreply.github.com>
2018-06-22 17:31:42 +0800

    Update color.sh

--- color.sh
+++ color.sh
@@ -1,5 +1,5 @@
-
-n rmsg() { echo -e "\033[31;49m$*\033[0m"; }
+#!/bin/bash
+function rmsg() { echo -e "\033[31;49m$*\033[0m"; }
   3 gmsg() { echo -e "\033[32;49m$*\033[0m"; }
   4 bmsg() { echo -e "\033[34;49m$*\033[0m"; }
   5 
