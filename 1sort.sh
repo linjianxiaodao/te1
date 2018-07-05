b1396fcbc8018ef989678828a8648a86694d553e
fumimi <fumm@haplox.com>
2018-07-05 08:56:27 +0800

    180705

---
+++ 1sort.sh
@@ -0,0 +1,3 @@
+java -jar /tools/GATK/picard/SortSam.jar\
+ INPUT=/haplox/users/fumimi/somatic/step5_dedup/S008_20180530020-c_ffpedna_86gene-180518_7111.mark.bam \
+ OUTPUT=./S008_20180530020-c_ffpedna_86gene-180518_7111.sort.bam SORT_ORDER=coordinate &
