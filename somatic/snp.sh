4e0e845a000340458a1480c8f2a00d995467365b
fumimi <fumm@haplox.com>
2018-07-06 17:28:47 +0800

    somatic

---
+++ somatic/snp.sh
@@ -0,0 +1,2 @@
+java -jar /tools/VarScan.v2.3.8.jar mpileup2snp /haplox/users/fumimi/somatic/step7_Mpileup/S008_20180530020-c_ffpedna_86gene-180518_7111.pilep --min-coverage 20 --min-var-freq 0.0005  --p-value 1   --output-vcf 1 > /haplox/users/fumimi/somatic/step8_VarScan2/S008_20180530020-c_ffpedna_86gene-180518_7111.snp.vcf
+
